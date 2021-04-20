836fb7da2f19c613f2b8b91a01e27368b4db8c58
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-20 09:28:28 -0700

    first commit, exchange_first_last() is complete. other functions still in work

--- slicing.py
+++ slicing.py
@@ -1,18 +1,23 @@
 def exchange_first_last(seq):
-    pass
-
+    new_seq = seq
+    first = seq[0]
+    last = seq[-1]
+    new_seq[0] = last
+    new_seq[-1] = first
+    return new_seq and print(new_seq)
 
 def every_other_removed(seq):
     pass
-
-
 def first4_last4_every_other_removed(seq):
     pass
-
-
 def seq_reversed(seq):
     pass
-
-
 def last_third_first_third_mid_third(seq):
     pass
+
+
+
+#Calling functions to test to see exchange_first_last(seq) works
+#seq = ['A','B','C','D','E','F']
+#seq =[1,2,3,4,5]
+#exchange_first_last(seq)
