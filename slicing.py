23ec375e1eedb75a4cd0d718cd963c9f0e8dbe2a
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-21 12:21:38 -0700

    The first three functions are complete and pass the assert tests

--- slicing.py
+++ slicing.py
@@ -13,14 +13,9 @@ def exchange_first_last(seq):
 	if isinstance(seq, str) == True:
 		seq_string = ''.join([str(elem) for elem in seq_list])
 		return seq_string
-
+	else:
+		return seq_list
 	pass
-
-a_string = "this is a string"
-a_tuple = (2, 54, 13, 12, 5, 32)
-
-assert exchange_first_last(a_string) == "ghis is a strint"
-assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
 
 def every_other_removed(seq):
 	seq_list = list(seq)  # convert tuple to list
@@ -33,17 +28,62 @@ def every_other_removed(seq):
 	if isinstance(seq, str) == True:
 		seq_string = ''.join([str(elem) for elem in seq_remove])
 		return seq_string
-
-removed_string=('ABCDEF')
-removed_tuple=(1, 2, 3, 4, 5, 6)
-
-assert every_other_removed(removed_string) == ('ACE')
-assert every_other_removed(removed_tuple) == (1, 3, 5)
+	else:
+		return seq_remove
 
 def first4_last4_every_other_removed(seq):
-    pass
+	seq_list = list(seq) #First convert input into list
+	if len(seq_list) < 10: #input needs to pass this test
+		return print('Input needs to have at least 10 characters')
+		exit()
+	else:
+		middle = seq_list[4:-4]
+		middle_every_other = middle[::2]
+
+	if isinstance(seq, tuple) == True: #if given an tuple we will need to return a tuple
+		seq_tuple = tuple(middle_every_other) #converting the list into tuple
+		return seq_tuple
+
+	if isinstance(seq, str) == True: #if given a string we will need to return a string
+		seq_string = ''.join([str(elem) for elem in middle_every_other])
+		return seq_string
+	else:
+		return middle_every_other
+	pass
+
 def seq_reversed(seq):
     pass
+
 def last_third_first_third_mid_third(seq):
     pass
 
+
+
+#Below are asserts I have written to test my code
+a_list = [1, 'A', 2, 'B', 3, 'C']
+a_string = "this is a string"
+a_tuple = (2, 54, 13, 12, 5, 32)
+assert exchange_first_last(a_list) == ['C', 'A', 2, 'B', 3, 1]
+assert exchange_first_last(a_string) == "ghis is a strint"
+assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+
+removed_list = [1, 'A', 2, 'B', 3, 'C']
+removed_string=('ABCDEF')
+removed_tuple=(1, 2, 3, 4, 5, 6)
+assert every_other_removed(removed_list) == [1, 2, 3]
+assert every_other_removed(removed_string) == ('ACE')
+assert every_other_removed(removed_tuple) == (1, 3, 5)
+
+
+#Below are asserst from the auto.py script
+assert exchange_first_last('something') == 'gomethins'
+assert exchange_first_last(list(range(10))) == [9, 1, 2, 3, 4, 5, 6, 7, 8, 0]
+assert exchange_first_last(tuple(range(10))) == (9, 1, 2, 3, 4, 5, 6, 7, 8, 0)
+
+assert every_other_removed('something') == 'smtig'
+assert every_other_removed(list(range(10))) == [0, 2, 4, 6, 8]
+assert every_other_removed(tuple(range(10))) == (0, 2, 4, 6, 8)
+
+assert first4_last4_every_other_removed('firstandlast') == 'tn'
+assert first4_last4_every_other_removed(list(range(10))) == [4]
+assert first4_last4_every_other_removed(tuple(range(10))) == (4,)
