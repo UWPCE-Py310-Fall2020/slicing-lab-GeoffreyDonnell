5257f8d6aad7e2e4db42bf9b9b524cf87c08f495
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-21 08:27:13 -0700

    exchange_first_last() function complete with assert test working

--- slicing.py
+++ slicing.py
@@ -16,9 +16,30 @@ def exchange_first_last(seq):
 
 	pass
 
+a_string = "this is a string"
+a_tuple = (2, 54, 13, 12, 5, 32)
+
+assert exchange_first_last(a_string) == "ghis is a strint"
+assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
 
 def every_other_removed(seq):
-	pass
+	seq_list = list(seq)  # convert tuple to list
+	seq_remove = seq_list[0::2]
+
+	if isinstance(seq, tuple) == True:
+		seq_tuple = tuple(seq_remove) #converting the list into tuple
+		return seq_tuple
+
+	if isinstance(seq, str) == True:
+		seq_string = ''.join([str(elem) for elem in seq_remove])
+		return seq_string
+
+removed_string=('ABCDEF')
+removed_tuple=(1, 2, 3, 4, 5, 6)
+
+assert every_other_removed(removed_string) == ('ACE')
+assert every_other_removed(removed_tuple) == (1, 3, 5)
+
 def first4_last4_every_other_removed(seq):
     pass
 def seq_reversed(seq):
@@ -26,8 +47,3 @@ def seq_reversed(seq):
 def last_third_first_third_mid_third(seq):
     pass
 
-a_string = "this is a string"
-a_tuple = (2, 54, 13, 12, 5, 32)
-
-assert exchange_first_last(a_string) == "ghis is a strint"
-assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
